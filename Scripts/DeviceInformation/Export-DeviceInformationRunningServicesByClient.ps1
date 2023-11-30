0a6f73061ba18b2d139d1da54a30c379f70cedb3
neo42JBR <julian.behr@neo42.de>
2023-11-30 17:05:18 +0100

    First round of new examples

---
+++ Scripts/DeviceInformation/Export-DeviceInformationRunningServicesByClient.ps1
@@ -0,0 +1,54 @@
+﻿#requires -version 5
+<#
+.SYNOPSIS
+	Get all the running services of a specific client
+.DESCRIPTION
+	An Example to show how to interact with the neo42 Management Service Api
+    to get all the running services of a specific client.
+.PARAMETER ServerName
+	The servername of the neo42 Management Service.
+.PARAMETER OutputPath
+	The path where the csv files should be stored.
+	Defaults to the script root.
+.PARAMETER Domain
+    The domain of the client.
+.PARAMETER ClientName
+    The name of the client.
+.OUTPUTS
+	none
+.NOTES
+	Version:        1.0
+	Author:         neo42 GmbH
+	Creation Date:  30.11.2023
+	Purpose/Change: Initial version
+.EXAMPLE
+	.\Export-DeviceInformationRunningServicesByClient.ps1 -ServerName "https://server.domain:4242" -Domain "domain" -ClientName "client"
+#>
+[CmdletBinding()]
+Param (
+	[Parameter(Mandatory = $true)]
+	[string]
+	$ServerName,
+	[Parameter(Mandatory = $false)]
+	[string]
+	$OutputPath = "$PSScriptRoot",
+    [parameter(Mandatory = $true)]
+    [String]
+    $Domain,
+    [parameter(Mandatory = $true)]
+    [String]
+    $ClientName
+)
+
+$clientUrl = "$ServerName/api/Client/$Domain/$ClientName"
+$deviceInformationSvcReportUrl = "$ServerName/api/DeviceInformationV2/{CLIENTID}"
+
+$filePath = Join-Path -Path $OutputPath -ChildPath "DeviceInformationServices.csv"
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("X-Neo42-Auth", "Admin")
+
+$client = Invoke-RestMethod -Method Get -Uri $clientUrl -Headers $headers -UseDefaultCredentials -ErrorAction Stop
+$services = Invoke-RestMethod -Method Get -Uri $deviceInformationSvcReportUrl.Replace("{CLIENTID}", $client.id) -Headers $headers -UseDefaultCredentials -ErrorAction Stop
+
+$services.OperatingSystem.Services | Where-Object { $_.Status -eq "Running" } | Select-Object -Property Name, DisplayName, StartType, StartUser | Export-Csv -Path $filePath -NoClobber -NoTypeInformation -Encoding Default
---
+++ Scripts/DeviceInformation/Export-InstalledNeoPackages.ps1
@@ -0,0 +1,60 @@
+﻿#requires -version 5
+<#
+.SYNOPSIS
+	Get all packages installed from the neo42 Package Depot
+.DESCRIPTION
+	An Example to show how to interact with the neo42 Management Service Api
+    to retrieve all packages installed from the neo42 Package Depot.
+.PARAMETER ServerName
+	The servername of the neo42 Management Service.
+.PARAMETER OutputPath
+	The path where the csv files should be stored.
+	Defaults to the script root.
+.OUTPUTS
+	none
+.NOTES
+	Version:        1.0
+	Author:         neo42 GmbH
+	Creation Date:  30.11.2023
+	Purpose/Change: Initial version
+.EXAMPLE
+	.\Export-InstalledNeoPackages.ps1 -ServerName "https://server.domain:4242"
+#>
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ServerName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $OutputPath = "$PSScriptRoot"
+)
+
+$filePath = Join-Path -Path $OutputPath -ChildPath "InstalledNeoPackages.csv"
+
+$clientUrl = "$ServerName/api/Client/$Domain/$ClientName"
+$deviceInformationSoftwareUrl = "$ServerName/api/DeviceInformationV2/?type=sw"
+
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("X-Neo42-Auth", "Admin")
+
+$clientsRaw = Invoke-RestMethod -Method Get -Uri $clientUrl -Headers $headers -UseDefaultCredentials -ErrorAction Stop
+$clientsDict = New-Object "System.Collections.Generic.Dictionary[[System.Guid],[System.Object]]"
+foreach ($client in $clientsRaw) {
+    $clientsDict.Add($client.Id, ($client | Select-Object -Property NetBiosName, Id))
+}
+
+$deviceInformationSoftware = Invoke-RestMethod -Method Get -Uri $deviceInformationSoftwareUrl -Headers $headers -UseDefaultCredentials -ErrorAction Stop
+
+$packages = @()
+$deviceInformationSoftware | ForEach-Object {
+    $client = $clientsDict[$_.Id]
+    if ($_.PSObject.Properties.Name -notcontains "Software") {
+        return
+    }
+    $filteredPackages = $_.Software | Where-Object { $true -eq $_.Key.startsWith("neoPackage") }
+    $packages += ($filteredPackages | Select-Object -Property DisplayName,  @{Name = 'Client'; Expression = { $clientsDict[$client.Id].NetBiosName } }, InstallDate, Version)
+}
+
+$packages | Export-Csv -Path $filePath -NoClobber -NoTypeInformation -Encoding Default
