fecdb1c1e3db6a2202ed49da1d7aa58c5338e079
Marius Gerighausen <marius.gerighausen@neo42.de>
2024-04-19 16:34:12 +0200

    add ConfigMgr Cmdlets from pipeline example

---
+++ Pipelines/ConfigMgr/Inline-ScriptContent.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    $AppName,
+    $CI_UniqueID,
+    $SiteServer,
+    $SiteCode
+)
+#Arguments to add to the Inline-Script PipelineTask: -AppName "Prefix_<Run.Developer> <Run.Product> <Run.Version>" -CI_UniqueID '<Run.ConfigMgrApplicationUniqueId>' -SiteServer "CM1.corp.contoso.com" -SiteCode "CHQ"
+#use your own SiteCode and SiteServer
+$ModelName = "$(($CI_UniqueID -split ":")[0])/$(($CI_UniqueID -split ":")[1])"
+Start-Process -PassThru -FilePath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList "-executionpolicy","bypass","-File","C:\neo42\ConfigMgrScripts\RenameCMApplication.ps1","-AppName","""$AppName""","-ModelName","$ModelName","-SiteServer","$SiteServer","-SiteCode","$SiteCode"
---
+++ Pipelines/ConfigMgr/RenameCMApplication.ps1
@@ -0,0 +1,30 @@
+﻿param(
+    $AppName,
+    $ModelName,
+    $SiteServer,
+    $SiteCode
+)
+
+# Customizations
+$initParams = @{}
+#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
+#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+# Do not change anything below this line
+
+# Import the ConfigurationManager.psd1 module 
+if((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+}
+
+# Connect to the site's drive if it is not already present
+if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $SiteServer @initParams
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SiteCode):\" @initParams
+
+# Uncomment for Logging next to the Scriptfile
+#Add-Content -Path "$PSScriptRoot\log.txt" -Value "$(Get-Date -Format '[dd.MM.yyyy HH:mm:ss]'): Rename App with ModelID $($ModelName) to $($AppName)"
+Set-CMApplication -ModelName $ModelName -NewName $AppName
