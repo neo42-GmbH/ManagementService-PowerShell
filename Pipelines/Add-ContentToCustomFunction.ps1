10026cfa1e7040e808b9c6ae1ba5a6ecabf94dd7
neoJBR <julian.behr@neo42.de>
2025-01-15 13:04:56 +0100

    add try catch with error output

--- Pipelines/Add-ContentToCustomFunction.ps1
+++ Pipelines/Add-ContentToCustomFunction.ps1
@@ -48,55 +48,61 @@ Param (
 	[switch]
 	$InsertAtEnd
 )
-## If the content is specified as a file, read the content from the file and use the content parameter
-if ($PSCmdlet.ParameterSetName -eq 'File') {
-	$Content = Get-Content -Path $ContentFile
-}
-## Import the Deploy-Application.ps1 file and validate it
-[System.Management.Automation.Language.ParseError[]]$errors = @()
-[System.Management.Automation.Language.Token[]]$tokens = @()
-[System.Management.Automation.Language.Ast]$ast = [System.Management.Automation.Language.Parser]::ParseFile($DeployApplicationFile.FullName, [ref]$tokens, [ref]$errors)
-if ($errors.Count -gt 0) {
-	throw "The file '$DeployApplicationFile' contains syntax errors: `n$($errors -join "`n")"
-}
-## Check if the content is valid PowerShell code
-[System.Management.Automation.Language.Parser]::ParseInput(($Content -join "`r`n"), [ref]$null, [ref]$errors) | Out-Null
-if ($errors.Count -gt 0) {
-	throw "The specified content has syntax errors. Please verify the input: `n$($errors -join "`n")"
-}
-## Find the custom function in the Deploy-Application.ps1 file
-[System.Management.Automation.Language.FunctionDefinitionAst]$customFunction = $ast.Find({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] -and $args[0].Name -eq $FunctionName }, $false)
-if ($null -eq $customFunction) {
-	throw "Custom function '$FunctionName' not found in '$DeployApplicationFile'"
-}
-## Insert the content at the specified position
-[string]$outputText = $ast.Extent.Text
-[string]$insertContent = "`r`n`t" + ($Content -join "`r`n`t") + "`r`n"
-if ($true -eq $InsertAtEnd) {
-	[scriptblock[]]$detectionBlocks = @(
-		{($tokens | Where-Object { $_.Kind -eq 'Comment' -and $_.Extent.Text -match "\s*\#endregion $($customFunction.Name) content\s*"} | Select-Object -First 1 -ExpandProperty Extent).StartOffset - 1},
-		{$customFunction.Body.Extent.EndOffset - 1}
-	)
-}
-else {
-	## Determine the extend after which to insert the content
-	[scriptblock[]]$detectionBlocks = @(
-		{($tokens | Where-Object { $_.Kind -eq 'Comment' -and $_.Extent.Text -match "\s*\#region $($customFunction.Name) content\s*"} | Select-Object -First 1 -ExpandProperty Extent).EndOffset},
-		{$customFunction.Find({ $args[0] -is [System.Management.Automation.Language.AssignmentStatementAst] -and $args[0].Left.Child.VariablePath.UserPath -eq 'script:installPhase' }, $true).Extent.EndOffset},
-		{$customFunction.Body.ParamBlock.Extent.EndOffset},
-		{$customFunction.Body.Extent.StartOffset + 1}
-	)
-}
-
-foreach ($block in $detectionBlocks) {
-	[int]$offset = . $block
-	if ($offset -gt 0) {
-		break
+try {
+	## If the content is specified as a file, read the content from the file and use the content parameter
+	if ($PSCmdlet.ParameterSetName -eq 'File') {
+		$Content = Get-Content -Path $ContentFile
 	}
-}
-if ($offset -eq 0) {
-	throw "Could not determine the position to insert the content in the custom function '$FunctionName'"
-}
+	## Import the Deploy-Application.ps1 file and validate it
+	[System.Management.Automation.Language.ParseError[]]$errors = @()
+	[System.Management.Automation.Language.Token[]]$tokens = @()
+	[System.Management.Automation.Language.Ast]$ast = [System.Management.Automation.Language.Parser]::ParseFile($DeployApplicationFile.FullName, [ref]$tokens, [ref]$errors)
+	if ($errors.Count -gt 0) {
+		throw "The file '$DeployApplicationFile' contains syntax errors: `n$($errors -join "`n")"
+	}
+	## Check if the content is valid PowerShell code
+	[System.Management.Automation.Language.Parser]::ParseInput(($Content -join "`r`n"), [ref]$null, [ref]$errors) | Out-Null
+	if ($errors.Count -gt 0) {
+		throw "The specified content has syntax errors. Please verify the input: `n$($errors -join "`n")"
+	}
+	## Find the custom function in the Deploy-Application.ps1 file
+	[System.Management.Automation.Language.FunctionDefinitionAst]$customFunction = $ast.Find({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] -and $args[0].Name -eq $FunctionName }, $false)
+	if ($null -eq $customFunction) {
+		throw "Custom function '$FunctionName' not found in '$DeployApplicationFile'"
+	}
+	## Insert the content at the specified position
+	[string]$outputText = $ast.Extent.Text
+	[string]$insertContent = "`r`n`t" + ($Content -join "`r`n`t") + "`r`n"
+	if ($true -eq $InsertAtEnd) {
+		[scriptblock[]]$detectionBlocks = @(
+			{($tokens | Where-Object { $_.Kind -eq 'Comment' -and $_.Extent.Text -match "\s*\#endregion $($customFunction.Name) content\s*"} | Select-Object -First 1 -ExpandProperty Extent).StartOffset - 1},
+			{$customFunction.Body.Extent.EndOffset - 1}
+		)
+	}
+	else {
+		## Determine the extend after which to insert the content
+		[scriptblock[]]$detectionBlocks = @(
+			{($tokens | Where-Object { $_.Kind -eq 'Comment' -and $_.Extent.Text -match "\s*\#region $($customFunction.Name) content\s*"} | Select-Object -First 1 -ExpandProperty Extent).EndOffset},
+			{$customFunction.Find({ $args[0] -is [System.Management.Automation.Language.AssignmentStatementAst] -and $args[0].Left.Child.VariablePath.UserPath -eq 'script:installPhase' }, $true).Extent.EndOffset},
+			{$customFunction.Body.ParamBlock.Extent.EndOffset},
+			{$customFunction.Body.Extent.StartOffset + 1}
+		)
+	}
 
-$outputText = $outputText.Insert($offset, $insertContent)
-Set-Content -Path $DeployApplicationFile.FullName -Value $outputText -Encoding UTF8 -NoNewline
+	foreach ($block in $detectionBlocks) {
+		[int]$offset = . $block
+		if ($offset -gt 0) {
+			break
+		}
+	}
+	if ($offset -eq 0) {
+		throw "Could not determine the position to insert the content in the custom function '$FunctionName'"
+	}
+
+	$outputText = $outputText.Insert($offset, $insertContent)
+	Set-Content -Path $DeployApplicationFile.FullName -Value $outputText -Encoding UTF8 -NoNewline
+}
+catch {
+	$Host.UI.WriteErrorLine((Out-String -InputObject $_ -Width ([System.Int32]::MaxValue)))
+	exit 1
+}
